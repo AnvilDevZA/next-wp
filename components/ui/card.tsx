eb8dcc288a7c160ec0dcc1d6ce5ca86f8030c499
AnvilDevZA <websmith@anvil-dev.site>
2025-08-11 12:15:02 +0200

    typescript...again?

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,4 +1,4 @@
-export default function Card({ cardTitle: any, cardText: any, buttonText: any }) {
+export default function Card({ cardTitle, cardText, buttonText }) {
   return (
     <div className="card bg-base-100 w-96 shadow-sm border mt-6">
       <figure className="px-6 pt-6">
